0eec80e772d24637489863bc28958659b34fc3fd
Vitaliy Belyaev <belyaev1392@gmail.com>
2023-01-12 13:43:17 +0600

    Complete challenges for Project6b

--- Project6b/Project6b/ViewController.swift
+++ Project6b/Project6b/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -48,31 +48,29 @@ class ViewController: UIViewController {
         view.addSubview(label4)
         view.addSubview(label5)
         
-//        let viewsDict = ["label1": label1,"label2": label2,"label3": label3,"label4": label4,"label5": label5]
-//
-//        for label in viewsDict.keys {
-//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", metrics: nil, views: viewsDict))
-//
-//        }
-//
-//        let metrics = ["labelHeight": 88]
-//
-//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", metrics: metrics, views: viewsDict))
+        //        let viewsDict = ["label1": label1,"label2": label2,"label3": label3,"label4": label4,"label5": label5]
+        //
+        //        for label in viewsDict.keys {
+        //            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", metrics: nil, views: viewsDict))
+        //
+        //        }
+        //
+        //        let metrics = ["labelHeight": 88]
+        //
+        //        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", metrics: metrics, views: viewsDict))
         
         let labels = [label1, label2, label3, label4, label5]
         var prev: UILabel?
         
         for label in labels {
             label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            
             label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
             
-            
-            let labelHeight = view.frame.height / 5 - 10
-            
-            label.heightAnchor.constraint(equalToConstant: labelHeight).isActive = true
+            label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.2, constant: -8).isActive = true
             
             if let prev = prev {
-                label.topAnchor.constraint(equalTo: prev.bottomAnchor, constant: 10).isActive = true
+                label.topAnchor.constraint(equalTo: prev.bottomAnchor, constant: 10).isActive = true
             } else {
                 label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
             }
@@ -80,7 +78,8 @@ class ViewController: UIViewController {
             prev = label
         }
     }
-
-
+    
+    
+    
 }
 
