8fe6ea21d6defffaf743124e0107ee4bc81a762b
Vitaliy Belyaev <belyaev1392@gmail.com>
2023-02-05 21:25:55 +0600

    Add WeSplit actual code with challenge

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,17 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    @FocusState private var isAmountFocused: Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalAmount: Double {
+        return (checkAmount + checkAmount * Double(tipPercentage) / 100)
+    }
+    
+    var totalPerPerson: Double {
+        return totalAmount / Double(numberOfPeople)
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, iOS!")
-                .foregroundColor(Color.red)
-                .font(Font.largeTitle)
-                .padding(EdgeInsets.init(top: 24, leading: 0, bottom: 0, trailing: 0))
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: getCurrencyCode()))
+                        .keyboardType(.decimalPad)
+                        .focused($isAmountFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<20, id: \.self) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip percantage", selection: $tipPercentage) {
+                        ForEach(0..<101, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                
+                Section {
+                    Text(totalAmount, format: .currency(code: getCurrencyCode()))
+                } header: {
+                    Text("Total amount")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: getCurrencyCode()))
+                } header: {
+                    Text("Amount per person")
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        isAmountFocused = false
+                    }
+                }
+            }
         }
-        .padding()
+    }
+    
+    private func getCurrencyCode() -> String {
+        Locale.current.currency?.identifier ?? "USD"
     }
 }
 
