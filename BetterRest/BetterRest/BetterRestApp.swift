a2613e74b3f26a34ee3d96330eb6542f4d6c0a0b
Vitaliy Belyaev <belyaev1392@gmail.com>
2023-02-24 22:10:45 +0600

    Add initial code with sleepAmount Stepper in BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Vitaliy on 24.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
