042ae0605d6ffa05c0fb6ef617403a7f0839c38c
Vitaliy Belyaev <belyaev1392@gmail.com>
2023-02-26 17:12:37 +0600

    Complete wrap up challenge for BetterRest

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -12,52 +12,15 @@ struct ContentView: View {
     
     @State private var sleepAmount = 8.0
     
-    @State private var wakeUpDate = Date.now
+    @State private var wakeUpDate = defaultWakeTime
     
     @State private var coffeeAmount = 1
     
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showAlert = false
-    
-    
-    var body: some View {
         
-        NavigationView {
-            VStack {
-                Text("When do want to wake up?")
-                    .font(.headline)
-                
-                DatePicker("Enter a time", selection: $wakeUpDate,displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                
-                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-                
-                Text("How much coffee cups you drink in a day?")
-                    .font(.headline)
-                
-                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
-                
-                Spacer()
-            }
-            .padding()
-            .navigationTitle("BetterRest")
-            .toolbar {
-                Button("Calcualte", action: calculateBedtime)
-            }
-            .alert(alertTitle, isPresented: $showAlert){
-                Button("Ok") { }
-            } message: {
-                Text(alertMessage)
-            }
-        }
-        
-    }
-    
-    func calculateBedtime() {
+    var idealBedtime: Date {
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
@@ -68,18 +31,74 @@ struct ContentView: View {
             
             let prediction = try model.prediction(wake: Double(hourPartSeconds + minutePartSeconds), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
             
-            let sleepTime = wakeUpDate - prediction.actualSleep
-            
-            alertTitle = "Your ideal bedtime calculated"
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            return wakeUpDate - prediction.actualSleep
         } catch {
             alertTitle = "Error"
             alertMessage = "Sorry, error occured while calculating your bedtime"
+            showAlert = true
+            return Date.now
         }
-        
-        showAlert = true
     }
     
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
+    
+    
+    var body: some View {
+        
+        NavigationView {
+            
+            
+            Form {
+                Section {
+                    HStack {
+                        Text("When do want to wake up?")
+                        
+                        Spacer()
+                        DatePicker("Enter a time", selection: $wakeUpDate,displayedComponents: .hourAndMinute)
+                            .labelsHidden()
+                    }
+                }
+                
+                Section {
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                } header: {
+                    Text("Desired amount of sleep")
+                }
+                
+                Section {
+                    Picker("Number of cups", selection: $coffeeAmount) {
+                        ForEach(1...20, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                } header: {
+                    Text("How much coffee cups you drink in a day?")
+                }
+                
+                Section {
+                    Text("\(idealBedtime.formatted(date: .omitted, time: .shortened))")
+                        .bold()
+                } header: {
+                    Text("Your ideal bedtime is")
+                        .font(.headline)
+                }
+            }
+            
+            
+            .navigationTitle("BetterRest")
+            .alert(alertTitle, isPresented: $showAlert){
+                Button("Ok") { }
+            } message: {
+                Text(alertMessage)
+            }
+        }
+        
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
